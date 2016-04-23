fa68f73ca476a1479f518951c36612ea318fb2f5
Snaehild <Snaehild@users.noreply.github.com>
2016-04-23 13:03:03 -0400

    04/23/16

--- GameHaunt/wp-content/themes/crystalskull/functions.php
+++ GameHaunt/wp-content/themes/crystalskull/functions.php
@@ -1281,40 +1281,6 @@ function crystalskull_link_pages_args_prevnext_add($args)
     return $args;
 }
 
-/*-----------------------------------------------------------------------------------*/
-/*	Custom Login
-/*-----------------------------------------------------------------------------------*/
-function my_custom_login() {
-echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('stylesheet_directory') . '/login/custom-login-styles.css" />';
-}
-add_action('login_head', 'my_custom_login');
-function my_login_logo_url() {
-return get_bloginfo( 'url' );
-}
-add_filter( 'login_headerurl', 'my_login_logo_url' );
-function my_login_logo_url_title() {
-return 'GameHaunt';
-}
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
-/*-----------------------------------------------------------------------------------*/
-/*	Change Redirect URL
-/*-----------------------------------------------------------------------------------*/
-function admin_login_redirect( $redirect_to, $request, $user )
-{
-global $user;
-if( isset( $user->roles ) && is_array( $user->roles ) ) {
-if( in_array( "administrator", $user->roles ) ) {
-return $redirect_to;
-} else {
-return home_url();
-}
-}
-else
-{
-return $redirect_to;
-}
-}
-add_filter("login_redirect", "admin_login_redirect", 10, 3);
 
 /* Custom code goes above this line. */
 ?>
